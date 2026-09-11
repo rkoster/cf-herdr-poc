7073d6b28f3c1f4158acd047b51705cee467dc1f
rkoster <hi@rkoster.dev>
2026-09-11 11:25:01 +0200

    fix: align sandbox OpenCode config path

--- sandbox/start_test.go
+++ sandbox/start_test.go
@@ -51,7 +51,7 @@ func TestLauncherContract(t *testing.T) {
 		"export PATH=\"$BIN_DIR:$PATH\"",
 		`local bashrc="$HOME/.bashrc"`,
 		`SANDBOX_STATE_DIR="${SANDBOX_STATE_DIR:-/home/vcap/app/.sandbox-state}"`,
-		`export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$SANDBOX_STATE_DIR/config}"`,
+		`export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"`,
 		`export XDG_STATE_HOME="${XDG_STATE_HOME:-$SANDBOX_STATE_DIR/state}"`,
 		`export XDG_DATA_HOME="${XDG_DATA_HOME:-$SANDBOX_STATE_DIR/data}"`,
 		`export COLLIE_STATE_DIR="${COLLIE_STATE_DIR:-$XDG_STATE_HOME/collie}"`,
